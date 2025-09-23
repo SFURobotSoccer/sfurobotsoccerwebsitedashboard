e14bef22a2eb9dc670131c8408db3607f5fbe72a
SFURobotSoccer <39747774+SFURobotSoccer@users.noreply.github.com>
2025-09-22 20:39:39 -0400

    Update Join.js
    removal of open roles

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -24,136 +24,140 @@ export const Join = () => {
     //   descriptionLink: null,
     //   goTo: "/join",
     // },
-    {
-      name: "VP of Internal Relations",
-      description:
-        "",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
-    {
-      name: "Outreach Co-Lead",
-      description:
-        "",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
-    {
-      name: "Finance Coordinator",
-      description:
-        "",
+    
+    //Fall 25
+    // {
+    //   name: "VP of Internal Relations",
+    //   description:
+    //     "",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
+    // {
+    //   name: "Outreach Co-Lead",
+    //   description:
+    //     "",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
+    // {
+    //   name: "Finance Coordinator",
+    //   description:
+    //     "",
         //"As the Finance Coordinator, you will manage the team’s budget, track expenses, and ensure compliance with funding guidelines. This role is suited for individuals with a strong grasp of financial principles and Excel skills, who are ready to contribute to the team’s financial transparency and success.",
-      linkApply:
-        "",
-      //"https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
-    {
-      name: "Corporate Relations Coordinator",
-      description:
-        "",
+    //   linkApply:
+    //     "",
+    //   //"https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
+    // {
+    //   name: "Corporate Relations Coordinator",
+    //   description:
+    //    "",
       //"As the Outreach Coordinator, you will inspire the next generation of engineers by organizing robotics workshops and events at local high schools. This role is perfect for individuals passionate about education and who excel in making complex concepts accessible and exciting.",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
-    {
-      name: "Outreach Coordinator",
-      description:
-        "",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
+    // {
+    //   name: "Outreach Coordinator",
+    //   description:
+    //     "",
         //"As the Outreach Coordinator, you will inspire the next generation of engineers by organizing robotics workshops and events at local high schools. This role is perfect for individuals passionate about education and who excel in making complex concepts accessible and exciting.",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
-    {
-      name: "Marketing Coordinator",
-      description:
-        "",
-       linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
-    {
-      name: "Learning Groups Coordinator",
-      description:
-        "",
-       linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
-      descriptionLink: null,
-      goTo: "/join",
-    },
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
+    // {
+    //   name: "Marketing Coordinator",
+    //   description:
+    //     "",
+    //    linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
+    // {
+    //   name: "Learning Groups Coordinator",
+    //   description:
+    //     "",
+    //    linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLScTyieZBFZa2BNwD5Ab6T-dOWGFJDDai-t1RPMpnSvkPVGY-w/viewform?usp=preview",
+    //   descriptionLink: null,
+    //   goTo: "/join",
+    // },
   ];
 
   const openRolesTech = [
-          {
-        name: "Mechanics Co-Lead",
-        description:
-          "As the Mechanics Co-Lead, you will guide the team in designing and developing the physical components of our robots, including chassis and drive systems. This leadership role is suited for individuals with a strong background i",
-        linkApply:
-          "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-        descriptionLink: null,
-      },
-      {
-        name: "Head of Mechanical Engineering",
-        description:
-          "As the Head of Mechanical Engineering, you will lead the team in designing and implementing the physical components of our robots, including the drive train, chassis, and kicker. You will provide technical guidance, delegate tasks effectively, and ensure the integration of mechanical designs with other subsystems. This leadership role is ideal for individuals with strong CAD skills, mechanical design expertise, and excellent organizational abilities.",
-        linkApply:
-          "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-        descriptionLink: null,
-      },
-    {
-        name: "Head of Electronics Engineering",
-        description:
-          "As Electronics Head Engineer, you’ll design and troubleshoot PCBs, mentor subteam members, and oversee integration of electronics with mechanics and firmware.",
-        linkApply:
-          "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-        descriptionLink: null,
-      },
-    {
-      name: "Firmware Developer",
-      description:
-        "Developing firmware for a range of microcontrollers including SAM, PIC, and Arduino platforms. Perform testing of firmware code on the robots and robotic components Involves contributing to code reviews, debugging, and optimizing firmware Integration testing across all robotic subsystems with other teams.",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-      descriptionLink: null,
-      goTo: "/teams/firmware",
-    },
-    {
-      name: "Mechanics Engineer",
-      description:
-        "Help design and prototype mechanical and electromechanical systems on our fleet of robots. Fabrication and assembly of parts including drive train, chassis, and kicker Research and evaluate materials essential for the projects, ensuring consistent improvement of mechanical quality",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-      descriptionLink: null,
-      goTo: "/teams/mechanics",
-    },
-    {
-      name: "Electronics Engineer",
-      description:
-        "Design and modify custom PCBs and other electronics using Altium designer. Actively engage in troubleshooting hardware issues. Work collaboratively with other members to integrate new components on the robots. Solder, assemble, and rework PCBs",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-      descriptionLink: null,
-      goTo: "/teams/electronics",
-    },
-    {
-      name: "Software Developer",
-      description:
-        "Build and test software components in C++, Python, QML, and XML. Contribute to multiple sub-projects and maintain. detailed documentation in a team setting. Write tests with Boost.UT and create documentation",
-      linkApply:
-        "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
-      descriptionLink: null,
-      goTo: "/teams/software",
-    },
+    
+    // Fall 25
+    //       {
+    //     name: "Mechanics Co-Lead",
+    //     description:
+    //       "As the Mechanics Co-Lead, you will guide the team in designing and developing the physical components of our robots, including chassis and drive systems. This leadership role is suited for individuals with a strong background i",
+    //     linkApply:
+    //       "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //     descriptionLink: null,
+    //   },
+    //   {
+    //     name: "Head of Mechanical Engineering",
+    //     description:
+    //       "As the Head of Mechanical Engineering, you will lead the team in designing and implementing the physical components of our robots, including the drive train, chassis, and kicker. You will provide technical guidance, delegate tasks effectively, and ensure the integration of mechanical designs with other subsystems. This leadership role is ideal for individuals with strong CAD skills, mechanical design expertise, and excellent organizational abilities.",
+    //     linkApply:
+    //       "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //     descriptionLink: null,
+    //   },
+    // {
+    //     name: "Head of Electronics Engineering",
+    //     description:
+    //       "As Electronics Head Engineer, you’ll design and troubleshoot PCBs, mentor subteam members, and oversee integration of electronics with mechanics and firmware.",
+    //     linkApply:
+    //       "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //     descriptionLink: null,
+    //   },
+    // {
+    //   name: "Firmware Developer",
+    //   description:
+    //     "Developing firmware for a range of microcontrollers including SAM, PIC, and Arduino platforms. Perform testing of firmware code on the robots and robotic components Involves contributing to code reviews, debugging, and optimizing firmware Integration testing across all robotic subsystems with other teams.",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //   descriptionLink: null,
+    //   goTo: "/teams/firmware",
+    // },
+    // {
+    //   name: "Mechanics Engineer",
+    //   description:
+    //     "Help design and prototype mechanical and electromechanical systems on our fleet of robots. Fabrication and assembly of parts including drive train, chassis, and kicker Research and evaluate materials essential for the projects, ensuring consistent improvement of mechanical quality",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //   descriptionLink: null,
+    //   goTo: "/teams/mechanics",
+    // },
+    // {
+    //   name: "Electronics Engineer",
+    //   description:
+    //     "Design and modify custom PCBs and other electronics using Altium designer. Actively engage in troubleshooting hardware issues. Work collaboratively with other members to integrate new components on the robots. Solder, assemble, and rework PCBs",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //   descriptionLink: null,
+    //   goTo: "/teams/electronics",
+    // },
+    // {
+    //   name: "Software Developer",
+    //   description:
+    //     "Build and test software components in C++, Python, QML, and XML. Contribute to multiple sub-projects and maintain. detailed documentation in a team setting. Write tests with Boost.UT and create documentation",
+    //   linkApply:
+    //     "https://docs.google.com/forms/d/e/1FAIpQLSc-b423mbwLRkFVAT65FnJ12MS8A6forlrx4sAF_gnLMmj4pw/viewform?usp=sharing&ouid=103666114779303861294",
+    //   descriptionLink: null,
+    //   goTo: "/teams/software",
+    // },
     // {
     //   name: "Mechanics Co-Lead",
     //   description:
@@ -246,14 +250,14 @@ export const Join = () => {
 
       <h3 className="plus-jakarta-sans-heading text-2xl mb-0">Open Roles</h3>
       
-        <a 
-          href="/SFURSFallHiringPackage2025-2026.pdf" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="underline font-bold"
-        >
-          View Full Hiring Package
-        </a>
+        // <a 
+        //   href="/SFURSFallHiringPackage2025-2026.pdf" 
+        //   target="_blank" 
+        //   rel="noopener noreferrer"
+        //   className="underline font-bold"
+        // >
+        //   View Full Hiring Package
+        // </a>
             
         <p className="plus-jakarta-sans-body text-black w-11/12 text-sm lg:mb-5 text-opacity-40 leading-6"></p>
         <div className="flex flex-row items-center gap-4 mb-5">
